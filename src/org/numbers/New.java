84acf62e669635084badeacf18966390c9c71396
arun1997sr <arunsrkumar1997@gmail.com>
2021-09-30 18:24:14 +0530

    thirdfloor

--- src/org/numbers/New.java
+++
@@ -1,5 +0,0 @@
-package org.numbers;
-
-public class New {
-
-}
---
+++ src/org/thirdfloor/PlusOne.java
@@ -0,0 +1,19 @@
+package org.thirdfloor;
+
+public class PlusOne {
+	
+	public void classA() {
+		System.out.println("arun kumar");
+
+	}
+	public void classB() {
+		System.out.println("ajeeth kumar");
+
+	}
+	public static void main(String[] args) {
+		PlusOne p=new PlusOne();
+		p.classA();
+		p.classB();
+	}
+
+}
