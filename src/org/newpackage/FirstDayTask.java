5c4032fff40f7ff75aa52431639d0b6916582c5e
Thiru525 <thiru525maran@gmail.com>
2021-09-30 20:16:26 +0530

    new

--- src/org/newpackage/FirstDayTask.java
+++ src/org/newpackage/FirstDayTask.java
@@ -1,5 +1,8 @@
 package org.newpackage;
 
 public class FirstDayTask {
-
+public static void main(String[] args) {
+	
+	
+}
 }
--- src/org/newpackage/FirstTest.java
+++ src/org/newpackage/FirstTest.java
@@ -1,5 +1,17 @@
 package org.newpackage;
 
+import java.util.Scanner;
+
 public class FirstTest {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int i = s.nextInt();
+	System.out.println("Integer" +i);
+	String a = s.next();
+	
+}
+
 
 }
+
+
