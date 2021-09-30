45391f5cd1c835cbf43d7a0bed56ebceb915e247
arun1997sr <arunsrkumar1997@gmail.com>
2021-09-30 18:47:19 +0530

    new class

---
+++ src/org/secondfloor/PlusOne.java
@@ -0,0 +1,19 @@
+package org.secondfloor;
+
+public class PlusOne {
+	
+	public  void classA() {
+		
+	     System.out.println("arun");
+	}
+	public  void classB() {
+		
+	     System.out.println("ajith");
+	}
+	public static void main(String[] args) {
+		PlusOne p=new PlusOne();
+		p.classA();
+		p.classB();
+	}
+
+}
