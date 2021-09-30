9efbfdd1e61fce519c228ad7042805658b4fdae4
vjkmr0609 <vijayviswa95@gmail.com>
2021-09-30 11:11:41 +0530

    new class vijay added

---
+++ src/org/firstfloor/VijayDemo.java
@@ -0,0 +1,16 @@
+package org.firstfloor;
+
+public class VijayDemo {
+
+	public void vijayId() {
+		System.out.println("12345");
+	}
+	public void vijayMail() {
+		System.out.println("vjkmr0609@gmail.com");
+	}
+public static void main(String[] args) {
+	VijayDemo vi= new VijayDemo();
+	vi.vijayId();
+	vi.vijayMail();
+}
+}
