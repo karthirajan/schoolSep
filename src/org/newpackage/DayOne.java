3c5f44cbee3e8147029f93dc75c38f00acf47a11
BalakannanAt21 <kannabala1995@gmail.com>
2021-10-01 08:40:08 +0530

    changes included

--- src/org/newpackage/DayOne.java
+++ src/org/newpackage/DayOne.java
@@ -1,5 +1,13 @@
 package org.newpackage;
 
 public class DayOne {
-
+	 
+	public void firstCode () {
+	 
+		System.out.println("Hello guys");
+}
+	public static void main(String[] args) {
+		DayOne d = new DayOne();
+		d.firstCode();
+	}
 }
