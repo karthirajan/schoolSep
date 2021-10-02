af3d5e723dbbe29293b69f46eb648e4f1602f5c6
Sivaraja27 <91624330+Sivaraja27@users.noreply.github.com>
2021-10-02 12:00:35 +0530

    One Project  File Added

---
+++ Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+    public void modelname() {
+    	System.out.println("emp ID()");
+    	}
+    
+    public void size() {
+    	System.out.println("emp Name()");
+    }
+        public void size1() {
+        	System.out.println("emp DOB()");   
+        }
+        	public void size2() {
+            	System.out.println("emp Phone()"); 
+        	}
+            	public void size3() {
+                	System.out.println("emp email()");    	
+            	}
+            	public static void main(String[]args) {
+          //syntax
+          // classname objname = new Classname();
+            	Employee a = new Employee();
+            	a.modelname();
+            	a.size();
+            	a.size1();
+            	a.size2();
+            	a.size3();
+            	
+            	}
+}
+
+            	
